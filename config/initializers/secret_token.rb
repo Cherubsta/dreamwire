0866d5643a4032bf9d6991bfb662a0fd24db3e16
Omar Hijaz <omarhijaz@gmail.com>
2013-06-18 21:51:52 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dreamwire::Application.config.secret_token = 'ed2d7de03c292c098e89a2f1c3606b574ae906ae60a0b5770f6ed6c13865118f559a309de7f2aa5a5ddc7c5454ff9361250a096c4f985138b27479183d8a0836'
