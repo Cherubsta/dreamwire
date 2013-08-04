932544f11e5011c4fc9c4049671543c5ee0f803d
Omar Hijaz <omarhijaz@gmail.com>
2013-08-04 00:33:25 -0400

    fix privacy issues

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,10 +3,10 @@ class Pin < ActiveRecord::Base
                   :fragmented, :hashtag, :user_id
   belongs_to :user
   
-  #validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 8000 }
-
+  validates :title, presence: true, length: { maximum: 36 }
   validates_inclusion_of :privacy, :in => [true, false]
+  # validates :privacy, presence: true
 
   # for sorting newest posts first
   default_scope order: 'pins.created_at DESC'
