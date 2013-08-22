8c44ea5e1b68016e1ab71092881e73cc837d8156
Omar Hijaz <omarhijaz@gmail.com>
2013-08-22 01:15:33 -0400

    add callback file for 500px, update routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Dreamwire::Application.routes.draw do
   match '/signout',            to: 'sessions#destroy', via: :delete
 
   get 'home' => 'pages#home'
+  get 'callback' => 'pages#callback'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
